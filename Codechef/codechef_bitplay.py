80b29cc1b4f1fb095a000f2c5864d67c7031278e
Amal Thomas <94557654+amalthomas2003@users.noreply.github.com>
2023-10-04 17:26:02 +0530

    Update codechef_bitplay.py

--- Codechef/codechef_bitplay.py
+++ Codechef/codechef_bitplay.py
@@ -1,18 +1,20 @@
+
+#https://www.codechef.com/problems/BITPLAY?tab=statement
+#difficulty:5/10
+#tags:bitwise operators
+
 for _ in range(int(input())):
     n=int(input())
     list1=[int (x) for x in list(input())]
     count=0
-    #print(list1)
+    temp_var=1
     for i in range(0,n-2,2):
-        temp_count=count
-        print(list1[i] & list1[i+1])
+        count=0
         if list1[i]&list1[i+1]==list1[i+2]:
             count+=1
         if list1[i]|list1[i+1]==list1[i+2]:
             count+=1
         if list1[i]^list1[i+1]==list1[i+2]:
             count+=1
-        if temp_count==count:
-            print(0)
-            break
-    print(count)
+        temp_var*=count
+    print(temp_var % 1000000007 )
