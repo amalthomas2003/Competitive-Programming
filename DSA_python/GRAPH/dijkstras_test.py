47c68b0710e4d77a9f7c2043c9230093986c4dd4
Amal Thomas <94557654+amalthomas2003@users.noreply.github.com>
2024-09-21 22:42:33 +0530

    Add files via upload

--- DSA_python/GRAPH/dijkstras_test.py
+++ DSA_python/GRAPH/dijkstras_test.py
@@ -10,12 +10,20 @@ graph=[
 ]
 
 #graph with negative cycles
-graph=[
+graph1=[
     [0,float('inf'),6,-6,float('inf')],
     [3,0,float('inf'),float('inf'),float('inf')],
     [float('inf'),float('inf'),0,1,float('inf')],
     [float('inf'),1,float('inf'),0,float('inf')],
     [float('inf'),float('inf'),float('inf'),float('inf'),0]
+]
+
+graph = [
+    [0, 3, float('inf'), 7, 8],  # A
+    [3, 0, 4, 2, float('inf')],  # B
+    [float('inf'), 4, 0, 5, 6],  # C
+    [7, 2, 5, 0, 1],             # D
+    [8, float('inf'), 6, 1, 0]   # E
 ]
 
 
@@ -40,6 +48,19 @@ def disjkstra(node):
                     prev_nodes[index]=curr_node
             heapq.heappush(heap,(min_distance[index],index))
     print(min_distance)
+    def printresult(prev_nodes,node,string):
+        if node==None:
+            return string
+        string+=str(node)
+        return printresult(prev_nodes,prev_nodes[node],string)
+        
+        
+
+
+    for i in range(len(prev_nodes)):
+        print("->".join(list(printresult(prev_nodes,i,"")[::-1])))
+
+
 
 
 disjkstra(0)
